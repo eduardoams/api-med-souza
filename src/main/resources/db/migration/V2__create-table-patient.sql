61191efe299b6a25c3fff3818f9c9ffac0684790
Eduardo Souza <eduardoa.ms14@gmail.com>
2023-10-30 08:12:58 -0300

    feat: criação de tabelas doctor e patient via migration

---
+++ src/main/resources/db/migration/V2__create-table-patient.sql
@@ -0,0 +1,16 @@
+create table tb_patient(
+    id bigint not null auto_increment,
+    name varchar(100) not null,
+    email varchar(100) not null unique,
+    telephone varchar(20) not null,
+    cpf varchar(11) not null unique,
+    public_place varchar(100) not null,
+    neighborhood varchar(100) not null,
+    cep varchar(9) not null,
+    complement varchar(100),
+    number varchar(20),
+    uf char(2) not null,
+    city varchar(100) not null,
+
+    primary key(id)
+);
