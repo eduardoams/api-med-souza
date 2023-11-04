4106c57b9b744553539bc9c36f9bae4d5fd68095
Eduardo Souza <eduardoa.ms14@gmail.com>
2023-11-04 13:17:10 -0300

    feat: implementação dos endpoints para listagem, atualização e exclusão lógica de médicos e pacientes

--- src/main/java/med/souza/api/controller/DoctorController.java
+++ src/main/java/med/souza/api/controller/DoctorController.java
@@ -1,15 +1,15 @@
 package med.souza.api.controller;
 
 import jakarta.validation.Valid;
-import med.souza.api.doctor.Doctor;
-import med.souza.api.doctor.DoctorRepository;
-import med.souza.api.doctor.DoctorSaveData;
+import med.souza.api.doctor.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/doctors")
@@ -22,5 +22,24 @@ public class DoctorController {
     @Transactional
     public void save(@RequestBody @Valid DoctorSaveData data) {
         repository.save(new Doctor(data));
+    }
+
+    @GetMapping
+    public Page<DoctorListingData> list(@PageableDefault(size = 1, sort = {"name"}) Pageable p) {
+        return repository.findAllByActiveTrue(p).map(DoctorListingData::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void update(@RequestBody @Valid DoctorUpdateData data) {
+        Doctor doctor = repository.getReferenceById(data.id());
+        doctor.update(data);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void delete(@PathVariable Long id) {
+        Doctor doctor = repository.getReferenceById(id);
+        doctor.delete();
     }
 }
--- src/main/java/med/souza/api/controller/PatientController.java
+++ src/main/java/med/souza/api/controller/PatientController.java
@@ -1,15 +1,12 @@
 package med.souza.api.controller;
 
 import jakarta.validation.Valid;
-import med.souza.api.patient.Patient;
-import med.souza.api.patient.PatientRepository;
-import med.souza.api.patient.PatientSaveData;
+import med.souza.api.patient.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/patients")
@@ -22,5 +19,24 @@ public class PatientController {
     @Transactional
     public void save(@RequestBody @Valid PatientSaveData data) {
         repository.save(new Patient(data));
+    }
+
+    @GetMapping
+    public Page<PatientListingData> list(Pageable p) {
+        return repository.findAllByActiveTrue(p).map(PatientListingData::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void update(@RequestBody @Valid PatientUpdateData data) {
+        Patient patient = repository.getReferenceById(data.id());
+        patient.update(data);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void delete(@PathVariable Long id) {
+        Patient patient = repository.getReferenceById(id);
+        patient.delete();
     }
 }
