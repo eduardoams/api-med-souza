a8e973200c6981d5221f797d8537eba34d7fc5f7
Eduardo Souza <eduardoa.ms14@gmail.com>
2023-11-08 08:05:36 -0300

    feat: tratamento personalizado de erros

---
+++ src/main/java/med/souza/api/exception/ErrorHandler.java
@@ -0,0 +1,31 @@
+package med.souza.api.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Void> error404() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ValidationErrorData>> error400(MethodArgumentNotValidException e) {
+        List<FieldError> errors = e.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map(ValidationErrorData::new).toList());
+    }
+
+    public record ValidationErrorData(String field, String message) {
+        public ValidationErrorData(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+}
