853054cda0342102e13297fa723481ac8a9ad837
Eduardo Souza <eduardoa.ms14@gmail.com>
2023-11-29 07:11:55 -0300

    feat: obtendo e-mail do usuário que está se autenticando

--- src/main/java/med/souza/api/infra/security/TokenService.java
+++ src/main/java/med/souza/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package med.souza.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.souza.api.domain.user.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -27,6 +28,19 @@ public class TokenService {
                     .sign(algorithm);
         } catch (JWTCreationException exception){
             throw new RuntimeException("Error generating token", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("API Med Souza")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException e) {
+            throw new RuntimeException("Invalid or expired JWT token");
         }
     }
 
