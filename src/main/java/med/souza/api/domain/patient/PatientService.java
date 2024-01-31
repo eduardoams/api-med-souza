f81678d453729bcc3e17b4c8f6bae8ea218bd80e
Eduardo Souza <eduardoa.ms14@gmail.com>
2024-01-31 08:04:41 -0300

    refactor: nomenclatura da exception de validação

--- src/main/java/med/souza/api/domain/patient/PatientService.java
+++ src/main/java/med/souza/api/domain/patient/PatientService.java
@@ -1,6 +1,6 @@
 package med.souza.api.domain.patient;
 
-import med.souza.api.domain.exception.IntegrityValidationException;
+import med.souza.api.domain.exception.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +14,6 @@ public class PatientService {
 
     public Patient findById(Long id) {
         Optional<Patient> patient = patientRepository.findById(id);
-        return patient.orElseThrow(() -> new IntegrityValidationException("Nenhum paciente encontrado com o ID " + id));
+        return patient.orElseThrow(() -> new ValidationException("Nenhum paciente encontrado com o ID " + id));
     }
 }
