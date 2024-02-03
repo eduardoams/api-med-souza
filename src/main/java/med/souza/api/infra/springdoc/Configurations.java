a7d0d37376403034f855acc3f91ee2ece0d7e758
Eduardo Souza <eduardoa.ms14@gmail.com>
2024-02-03 13:03:32 -0300

    feat: implementação de informações e contato no openapi

--- src/main/java/med/souza/api/infra/springdoc/Configurations.java
+++ src/main/java/med/souza/api/infra/springdoc/Configurations.java
@@ -2,6 +2,8 @@ package med.souza.api.infra.springdoc;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,9 +13,27 @@ public class Configurations {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        //Segurança
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        //Informações
+        String title = "API Med Souza";
+        String description = "API Rest da aplicação Med Souza, contendo as funcionalidades de CRUD de médicos e de pacientes, além de agendamento e cancelamento de consultas";
+
+        Contact contact = new Contact()
+                .name("Desenvolvedor")
+                .email("eduardoa.ms14@gmail.com");
+
+        Info info = new Info()
+                .title(title)
+                .description(description)
+                .contact(contact);
+
         return new OpenAPI()
-                .components(new Components()
-                        .addSecuritySchemes("bearer-key",
-                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+                .components(new Components().addSecuritySchemes("bearer-key", securityScheme))
+                .info(info);
     }
 }
